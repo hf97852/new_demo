11a73660671681c546eb0decb2ed336445a0aa7f
hf97852 <hf97852@users.noreply.github.com>
2024-04-03 00:03:50 +0800

    [Mod]第六段：单个合约策略

--- run.py
+++ run.py
@@ -2,10 +2,11 @@
 # from vnpy_tts.api.vnttsmd import MdApi
 from vnpy.event import EventEngine, Event
 from vnpy.trader.event import (EVENT_LOG, EVENT_TICK, EVENT_CONTRACT, EVENT_POSITION, EVENT_TIMER)
-from vnpy.trader.constant import Exchange
-from vnpy.trader.object import (TickData, LogData, SubscribeRequest, ContractData, PositionData)
+from vnpy.trader.constant import (Exchange, Direction, OrderType, Offset)
+from vnpy.trader.object import (TickData, LogData, SubscribeRequest, ContractData, PositionData, OrderRequest)
 from vnpy.trader.gateway import BaseGateway
-from vnpy_ctp import CtpGateway
+# from vnpy_ctp import CtpGateway
+from vnpy_tts import TtsGateway as CtpGateway
 
 from PySide6 import QtWidgets
 
@@ -95,20 +96,40 @@ class MonitorEngine:
         self.subscribed: set[str] = set()
 
         self.event_engine.register(EVENT_TICK, self.process_tick_event)        # on_tick在行情推送回报中,压入事件引擎的队列中, 订阅市场数据,触发订阅行情回报、与行情推送回报. 在subscribe函数中，启动订阅行情数据，并在订阅集合中追加订阅symbol.
-        self.event_engine.register(EVENT_CONTRACT, self.process_contract_event)  # 在合约查询回报函数中，压入事件引擎的队列中，供事件分发与处理
+        self.event_engine.register(EVENT_CONTRACT, self.process_contract_event)
+        # 在合约查询回报函数中（一个回报函数压入一个合约，难道请求合约查询后，分多次回调这个回报函数，逐个将所有合约压入队列中？答：估计一次请求查询合约，回调一次函数压入队列，即使合约查询完成，又while()重复循环查），压入事件引擎的队列中，供事件分发与处理
+
         self.event_engine.register(EVENT_POSITION, self.process_position_event)
-        self.event_engine.register(EVENT_TIMER, self.process_timer_event)  # 定时事件触发，再定时事件中触发：查询资金账户、持仓信息。
+        # self.event_engine.register(EVENT_TIMER, self.process_timer_event)  # 定时事件触发，再定时事件中触发：查询资金账户、持仓信息。
         self.event_engine.register(EVENT_LOG, self.process_log_event)
+
+        self.tick_history: list[TickData] = []
+        self.trading_symbol: str = "IF2405.CFFEX"
+        self.trading_target: int = 0
 
     def process_tick_event(self, event: Event) -> None:      # 而subscribe函数在持仓事件中订阅行情！
         """行情事件"""
         tick: TickData = event.data
         self.ticks[tick.vt_symbol] = tick      # 分别保存持仓合约的行情数据。
+
+        # 缓存交易合约的Tick历史
+        if tick.vt_symbol == self.trading_symbol:
+            self.tick_history.append(tick)
+
+            self.run_trading()
+
     def process_contract_event(self, event: Event) -> None:   # 交易服务器连接、登陆、结算信息确认成功——》请求查询合约——》查询合约回调，合约压入事件队列。
         # 说明：查询合约信息有多条，每条保存symbol_contract_map全局缓存字典中。
         """合约事件"""
         contract: ContractData = event.data
         self.contracts[contract.vt_symbol] = contract   # 通过事件引擎，把合约信息全部保存在 新的contracts字典中。这个由于后面查询合约，不再压入事件队列，不在更新？
+        # 【（答：估计while()循环查询合约，才能不停的订阅.trading_symbol的行情数据，才能解释tick数据大于3个，才能执行后面的买卖策略。）】
+
+        # 订阅策略合约行情
+        if contract.vt_symbol == self.trading_symbol:  # 请求一个合约、压入队列（此处一个循环），马上取出执行process_contract_event（一个线程中执行），接着压订阅行情、行情回报、压入队列，这个线程再从事件队列（含有EVENT_CONTRACT、EVENT_TICK）中，分发与处理事件。
+            req = SubscribeRequest(contract.symbol, contract.exchange)
+            self.gateway.subscribe(req)   # 查询合约、与交易合约的行情事件同时存在队列中，即队列含有EVENT_CONTRACT、EVENT_TICK。 （最终都会将合约放入contracts中，其中trading_symbol订阅行情数据同时保存在ticks、 tick_history中）
+
     def process_position_event(self, event: Event) -> None: # 说明：查询持仓信息有多条，每条保存positions（CtpTdApi类定义的，非这个MonitorEngine中定义的position)中,在压入持仓事件中，最后清空CtpTdApi类定义的positions字典。
         """持仓事件"""
         position: PositionData = event.data
@@ -152,7 +173,54 @@ class MonitorEngine:
             value = position.volume * tick.last_price * contract.size
             print(f"{position.vt_symbol} {position.direction}当前持仓市值{value}")
 
+    def run_trading(self) -> None:
+        """执行策略交易"""
+        # 检查至少要3个Tick
+        if len(self.tick_history) < 3:   # 从此处知道一个tick是一个切面行情数据。
+            return
+        # 提取行情和合约
+        tick1 = self.tick_history[-1]
+        tick2 = self.tick_history[-2]
+        tick3 = self.tick_history[-3]
 
+        contract = self.contracts[self.trading_symbol]
+
+        # print(tick1.datetime, tick1.last_price, self.trading_target)
+
+        # 多头检查
+        if tick1.last_price > tick2.last_price > tick3.last_price:
+            if not self.trading_target:
+                req = OrderRequest(
+                    symbol=contract.symbol,
+                    exchange=contract.exchange,
+                    direction=Direction.LONG,
+                    type=OrderType.LIMIT,
+                    price=tick1.last_price + 10,
+                    volume=1,
+                    offset=Offset.OPEN
+                )
+                self.gateway.send_order(req)
+
+                self.trading_target = 1
+                print(f"{self.trading_symbol}买入开仓1手", tick1.datetime)
+
+        # 空头检查
+        if tick1.last_price < tick2.last_price < tick3.last_price:
+            if self.trading_target > 0:
+                req = OrderRequest(
+                    symbol=contract.symbol,
+                    exchange=contract.exchange,
+                    direction=Direction.SHORT,
+                    type=OrderType.LIMIT,
+                    price=tick1.last_price - 10,
+                    volume=1,
+                    offset=Offset.CLOSE
+                )
+                self.gateway.send_order(req)
+
+                self.trading_target = 0
+                print(f"{self.trading_symbol}卖出平仓1手", tick1.datetime)
+                
 def main():
     """主函数"""
     # 创建并启动事件引擎
@@ -172,26 +240,27 @@ def main():
     # widget.show()
 
     # CTP交易接口
-    ctp_setting = {
-        "用户名": "000300",
-        "密码": "vnpy1234",
-        "经纪商代码": "9999",
-        "交易服务器": "180.168.146.187:10130",
-        "行情服务器": "180.168.146.187:10131",
-        "产品名称": "simnow_client_test",
-        "授权编码": "0000000000000000"
-    }
-
     # ctp_setting = {
-    #     "用户名": "3271",
-    #     "密码": "123456",
-    #     "经纪商代码": "",
-    #     "交易服务器": "tcp://122.51.136.165:20002",
-    #     "行情服务器": "tcp://122.51.136.165:20004",
-    #     "产品名称": "",
-    #     "授权编码": ""
+    #     "用户名": "000300",
+    #     "密码": "vnpy1234",
+    #     "经纪商代码": "9999",
+    #     "交易服务器": "180.168.146.187:10130",
+    #     "行情服务器": "180.168.146.187:10131",
+    #     "产品名称": "simnow_client_test",
+    #     "授权编码": "0000000000000000"
     # }
 
+    ctp_setting = {
+        "用户名": "3271",
+        "密码": "123456",
+        "经纪商代码": "",
+        "交易服务器": "tcp://121.37.80.177:20002",
+        "行情服务器": "tcp://121.37.80.177:20004",
+        "产品名称": "",
+        "授权编码": ""
+    }
+    # "交易服务器": "tcp://122.51.136.165:20002",
+    # "行情服务器": "tcp://122.51.136.165:20004",
 
 
     ctp_gateway = CtpGateway(event_engine, 'CTP')   # 实例ctp_gateway时，绑定CtpMdApi的实例，CtpMdApi实例又反过来绑定ctp_gateway实例。
